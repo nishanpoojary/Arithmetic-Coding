2599fbed934edb233e64e8dc758d60bdf516a89f
nishanpoojary <48196285+nishanpoojary@users.noreply.github.com>
2019-08-10 12:19:51 +0530

    Add files via upload

--- arithmetic_coding.py
+++ arithmetic_coding.py
@@ -3,10 +3,9 @@
 
 
 from collections import Counter
-from collections import OrderedDict
 import numpy as np
-import math
 
+print("Enter a Sequence\n")
 inputstr = input()
 print  (inputstr + "\n")
 
@@ -16,17 +15,12 @@ print (str(res))
 #sortlist = sorted(res.iteritems(), lambda x, y : cmp(x[1], y[1]), reverse = True)
 #print sortlist
 
-#od = OrderedDict(res)
-#print od
-
 M = len(res)
 #print (M)
 N = 5
 A = np.zeros((M,5),dtype=object)
-#print (A)
 
 #A = [[0 for i in range(N)] for j in range(M)]
-#print (A)
 
 reskeys = list(res.keys())
 #print (reskeys)
@@ -34,6 +28,7 @@ resvalue = list(res.values())
 totalsum = sum(resvalue)
 
 # Creating Table
+
 A[M-1][3] = 0
 for i in range(M):
    A[i][0] = reskeys[i]
@@ -48,17 +43,16 @@ while i < M-1:
 print (A)
 
 # Encoding
+
+print("\n------- ENCODING -------\n" )
 strlist = list(inputstr)
 LEnco = []
 UEnco = []
 LEnco.append(0)
 UEnco.append(1)
 
-for i in range(len(res)):
-    #print (reskeys[i])
-    result = np.where(A == reskeys[i])
-    #print (A[result[0],2])
-    #print (A.index(reskeys[i]))
+for i in range(len(strlist)):
+    result = np.where(A == reskeys[reskeys.index(strlist[i])])
     addtollist = (LEnco[i] + (UEnco[i] - LEnco[i])*float(A[result[0],3]))
     addtoulist = (LEnco[i] + (UEnco[i] - LEnco[i])*float(A[result[0],4]))
 
@@ -66,16 +60,22 @@ for i in range(len(res)):
     UEnco.append(addtoulist)
 
     tag = (LEnco[-1] + UEnco[-1])/2.0
-    print(tag)
+
+LEnco.insert(0, " Lower Range")
+UEnco.insert(0, "Upper Range")
+print(np.transpose(np.array(([LEnco],[UEnco]),dtype=object)))
+print("\nThe Tag is \n ")
+print(tag)
 
 # Decoding
 
+print("\n------- DECODING -------\n" )
 ltag = 0
 utag = 1
 decodedSeq = []
 for i in range(len(inputstr)):
     numDeco = ((tag - ltag)*1.0)/(utag - ltag)
-    print(numDeco)
+    #print(numDeco)
     for i in range(M):
         if (float(A[i,3]) < numDeco < float(A[i,4])):
 
@@ -84,6 +84,5 @@ for i in range(len(inputstr)):
             utag = float(A[i,4])
             tag = numDeco
 
-#print(decodedSeq)
 print("The decoded Sequence is \n ")
 print("".join(decodedSeq))
